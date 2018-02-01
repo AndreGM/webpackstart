562fed0c0ce5b868d78aced5e7c0dc11e92c539e
Andre Gomes <andre.gomes@2xt.com.br>
2018-02-01 19:29:55 -0200

    testing some changes

--- src/about.js
+++ src/about.js
@@ -1,6 +1,6 @@
 import "./scss/typography.scss";
 import { about } from './js/module_about';
-// import { shortcode } from './js/module_shortcode';
+import { shortcode } from './js/module_shortcode';
 console.log('root aboutjs loaded')
 about();
-// shortcode();
+shortcode();
